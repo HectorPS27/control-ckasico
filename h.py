66e36652aef4075738eec4ae9bc340765ad82857
HectorPS27 <y10368hs@gmail.com>
2025-09-15 16:57:17 -0600

    prueba2

--- h.py
+++ h.py
@@ -1,1 +1,10 @@
-print ("hola wolrd")
+import datetime
+import time
+
+hoy = datetime.datetime.today().weekday()  # 0 = lunes, 6 = domingo
+dias_faltan = (3 - hoy) % 7  # 3 es jueves
+segundos_faltan = dias_faltan * 24 * 60 * 60
+
+print(f"Esperando {dias_faltan} día(s) hasta el jueves...")
+time.sleep(segundos_faltan)
+print("Ya es jueves, salida programada ✅")
